b5d090dcb55b884c711ae6b1c11f4900aa595dec
abaddon101 <101891842+abaddon101@users.noreply.github.com>
2022-11-18 15:23:22 +0100

    mep boucle dans photographer.js => création d'un grid => finalité : ajout de section et d'artcile de façon automatique, refactorisation

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,11 +1,12 @@
 function photographerFactory(data, media) {
   const { city, country, name, tagline, portrait, price, id } = data;
   // création d'un objet contenant les éléments des medias
-  const {photographerId, title, image, video, likes, date  } = media;
+  const { photographerId, title, image, video, likes, date } = media;
   // console.log(price);
   const picture = `assets/photographersMini/${portrait}`;
-  const pictureMedia = `assets/FishEye_Photos/Sample Photos${name}`;
+  const pictureMedia = `assets/FishEye_Photos/Sample Photos${name}/${image}.jpg`;
   console.log(name);
+  console.log(pictureMedia);
   // la fonction getUserCardDOM permet :
   function getUserCardDOM() {
     const article = document.createElement("article");
@@ -71,53 +72,99 @@ function photographerFactory(data, media) {
     return headerSection;
   }
   // Creation du block page
-  function mediaCard() {
+  function getMediaCardDOM() {
+    console.log(getMediaCardDOM);
 
+    const main = document.querySelector("#main");
+    console.log(main)
+    const section = document.createElement("section");
+    section.className = "mediaSection"
 
-
-
-    console.log(mediaCard);
-    const blockPage = document.getElementById("main");
-    const mediaSection = document.createElement("div");
-    mediaSection.className = "mediaSection";
-    blockPage.appendChild(mediaSection);
-    const mediaFilter = document.createElement("div");
-    mediaFilter.className += "mediaFilter";
-    mediaSection.appendChild(mediaFilter);
-    const mediaCards = document.createElement("div");
-    mediaCards.className += "mediaCards";
-    mediaSection.appendChild(mediaCards);
-
-    // dans ma mediaCards je vais ajouter un article comprenant les éléments du photographe.
-    const articleMedia = document.createElement("article");
-    // je souhaite selectionner les dossiers
-    // const picture = `assets/FishEye_Photos/Sample Photos${name}`;
-    console.log(pictureMedia);
-    // l'imgMedia marche,
-    // maintenant je dois changer ça
-    // en créant une varibale capable de récupérer les photos
-    // du photographe grâce à l'id du photgraphe ou des photos.
-// création d'une fonction permettant de cibler les éléemnts jpg du tableau
-    let findImgJpgMedia = () => {
-
-    }
-    const imgMedia = document.createElement("img");
+    main.appendChild(section);
+    const article = document.createElement("article");
     
-    mediaCards.appendChild(articleMedia);
-    mediaCards.appendChild(imgMedia);
+    section.appendChild(article)
+    const imgMedia = document.createElement("img");
+    article.appendChild(imgMedia);
+    article.innerText = title;
     imgMedia.setAttribute("src", pictureMedia);
 
-    articleMedia.style.backgroundColor = "yellow";
+    /////     // fonction permettant au moment ou on clique dessus, de pouvoir accéder à la page du photographer.html
+    /////     article.onclick = function () {
+    /////       location.href = `photographer.html?id=${id}`;
+    /////     };
+    /////     const img = document.createElement("img");
+    /////     article.appendChild(img);
+    /////     img.style.borderRadius = "100%";
+    /////     img.style.objectFit = "cover";
+    /////     article.style.cursor = "pointer";
+    /////     img.setAttribute("src", picture);
+    /////     img.alt = name;
+    /////     const h2 = document.createElement("h2");
+    /////     h2.textContent = name;
+    /////     article.appendChild(h2);
+    /////     const divCityAndCountry = document.createElement("div");
+    /////     divCityAndCountry.textContent = city + ", " + country;
+    /////     article.appendChild(divCityAndCountry);
+    /////     divCityAndCountry.style.color = "#901c1c";
+    /////     const divTagLine = document.createElement("div");
+    /////     divTagLine.textContent = tagline;
+    /////     article.appendChild(divTagLine);
+    /////     divTagLine.style.color = "black";
+    /////     divTagLine.style.fontSize = "13px";
+    /////     const divPrice = document.createElement("div");
+    /////     divPrice.textContent = price + "€/jour";
+    /////     article.appendChild(divPrice);
+    /////     divPrice.style.color = "#4D4D4D";
+    /////     divPrice.style.fontSize = "13px";
+    /////     // retourne à l'article
+    return article;
 
-    articleMedia.innerText = "yooooo";
-    articleMedia.style.cursor = "pointer";
-    console.log(imgMedia);
+    // console.log(mediaCard);
+    // const blockPage = document.getElementById("main");
+    // const mediaSection = document.createElement("div");
+    //  mediaSection.className = "mediaSection";
+    // blockPage.appendChild(mediaSection);
+    // const mediaFilter = document.createElement("div");
+    // mediaFilter.className += "mediaFilter";
+    // mediaSection.appendChild(mediaFilter);
+    // const mediaCards = document.createElement("div");
+    //mediaCards.className += "mediaCards";
+    // mediaSection.appendChild(mediaCards);
+
+    // dans ma mediaCards je vais ajouter un article comprenant les éléments du photographe.
+    // const articleMedia = document.createElement("article");
+    // je souhaite selectionner les dossiers
+    // const picture = `assets/FishEye_Photos/Sample Photos${name}`;
+    //  console.log(pictureMedia);
+    // l'imgMedia marche,
+    // maintenant je dois changer ça
+    // en créant une variable capable de récupérer les photos
+    // du photographe grâce à l'id du photgraphe ou des photos.
+    // création d'une fonction permettant de cibler les éléemnts jpg du tableau
+
+    //   const imgMedia = document.createElement("img");
+    //   mediaCards.appendChild(articleMedia);
+    //   mediaCards.appendChild(imgMedia);
+    //   imgMedia.setAttribute("src", pictureMedia);
+
+    // articleMedia.style.backgroundColor = "yellow";
+
+    //   articleMedia.innerText = "yooooo";
+    //   articleMedia.style.cursor = "pointer";
+    //   console.log(imgMedia);
     // fonction permettant au moment ou on clique dessus, de pouvoir accéder à la page du photographer.html
     // articleMedia.onclick = function () {
     //   location.href = `photographer.html?id=${id}`;
     // };
   }
-  return { name, picture, getUserCardDOM, headerPhotographer, mediaCard };
+  return {
+    name,
+    picture,
+    getUserCardDOM,
+    getMediaCardDOM,
+    headerPhotographer,
+  };
 }
 
 // création d'un article, pour insérer les cardMedias
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -14,7 +14,6 @@ function getIdFromUrl() {
   const param1Id = urlParams.get("id");
   return param1Id;
 }
-
 async function init() {
   const paramId = getIdFromUrl();
   const photographerDisplayData = await getPhotographer(paramId);
@@ -23,39 +22,76 @@ async function init() {
   const media = await getMedia(paramId);
   displayData(photographerDisplayData, media);
   console.log(media);
+  media.forEach((photographer, media) => {
+    // const image = document.createElement("img");
+    // image.src = `assets/FishEye_Photos/Sample Photos${object.image}.jpg`;
+    console.log(photographer.image);
+    photographer.photographerId === 243 ||
+      photographer.photographerId === 930 ||
+      photographer.photographerId === 82 ||
+      photographer.photographerId === 527 ||
+      photographer.photographerId === 925 ||
+      photographer.photographerId === 195 ||
+      media.photographerId === 243 ||
+      media.photographerId === 930 ||
+      media.photographerId === 82 ||
+      media.photographerId === 527 ||
+      media.photographerId === 925 ||
+      media.photographerId === 195;
+    // console.log("image du photographe :", object.image);
+    // console.log("video du photographe :", object.video);
+    const photographerModel = photographerFactory(photographer, media);
+    const mediaSection = document.querySelector(".mediaSection");
+    const mediaCardDOM = photographerModel.getMediaCardDOM();
+    mediaSection.appendChild(mediaCardDOM);
+  });
+
   //  mediaCard(media);
 }
-init();
+// recupère les données du tableau  getMedia dans api.js
 
+init();
 async function displayData(photographer, media) {
+  // la constante headerSection selectionnne la class headerSection
+  const headerSection = document.querySelector(".photograph-header-container");
+  const mediaSection = document.querySelector(".mediaSection");
+
   // La const photographerModel appelle  photographerFactory avec comme argument photographer
   const photographerModel = photographerFactory(photographer, media);
+
+  console.log(photographerModel);
+
   // j'appel headerPhotographer grâce à photographerModel
   const headerphotographer = photographerModel.headerPhotographer();
-  const headerSection = document.querySelector(".photograph-header-container");
-  headerSection.appendChild(headerphotographer);
-  // j'appel mediaCard grâce à photographerModel
-   const mediaSection = photographerModel.mediaCard(photographer);
 
-  
-  
+  headerSection.appendChild(headerphotographer);
+  console.log(headerSection);
+
+  // j'appel mediaCard grâce à photographerModel
+  const mediaCard = photographerModel.getMediaCardDOM();
+
+  // mediaSectionPhotographer.appendChild(mediaCard);
+
+  // const mediaSection = document.querySelector(".mediaSection");
+  // const userCarMEdia = photographerModel.mediaCard();
+  // mediaSection.appendChild(userCarMEdia);
+  // console.log(userCarMEdia);
+
+  // mediaSection.appendChild(mediaCard)
   // dans mon médiaCard je veux insérer les medias img et video
   // par rapport au photgraphId du json et d'un ${}
-  
-
-
   //
 }
 
 // async function displayData(photographers) {
 //   // la constante photographersSection selectionnne la class photographer_section
 //   const photographersSection = document.querySelector(".photographer_section");
-// 
+//
 //   photographers.forEach((photographer) => {
 //     const photographerModel = photographerFactory(photographer);
 //     console.log(photographerModel);
 //     const userCardDOM = photographerModel.getUserCardDOM();
 //     photographersSection.appendChild(userCardDOM);
-//     
+//
 //   });
 // }
