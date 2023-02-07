a9b80dcc0c9692206eef7c36e37fd2bc666b499d
abaddon101 <101891842+abaddon101@users.noreply.github.com>
2023-02-07 12:20:26 +0100

    mep d'un visibility hidden et visible pour le footer lorsque la loghtbox est active puis desactive

--- scripts/utils/lightBox.js
+++ scripts/utils/lightBox.js
@@ -1,31 +1,6 @@
 /* eslint-disable no-undef */
-
-///  LightBox (le paramêtre récupère les medias de displayMedia)
 // eslint-disable-next-line no-unused-vars
 function lightBox() {
-  //  const { photographerId, title, image, video, likes, date } =
-  //    mediaList;
-  // console.log(mediaList);
-  // console.log(mediaList[(0, 1, 2, 3, 4, 5, 6)]);
-  // console.log(mediaList[1]);
-  // let contentTitle = mediaList;
-  // let infoPerso = photographs;
-  // console.log(infoPerso);
-  // console.log(contentTitle);
-  //  console.log(Object.values(contentTitle));
-  // const resulOfTheFinder = () => {
-  //   contentTitle.find((element) => {
-  //     console.log(element);
-  //     title = element.title;
-  //     console.log(title);
-  //   });
-  // };
-
-  // console.log(id);
-  // testParId
-  // resulOfTheFinder();
-  // console.log(resulOfTheFinder);
-
   const tabindex = 0;
   let lightBoxActiveElement = null;
   const lightboxmodal = document.querySelector(".lightboxModal");
@@ -68,19 +43,14 @@ function lightBox() {
   mediaLegend.appendChild(titlelightBoxImg);
 
   function playPauseMedia(play) {
-    // mediaVideo.play();
     play.setAttribute("data-icon", "P");
   }
 
   const selectObj = document.querySelectorAll(
     ".mediaSection img.mediaImage , .mediaSection video.movieMedia"
   );
-
-  // console.log(selectObj);
-
   /////////////  création du slider /////////////
   const numberObj = selectObj.length;
-  // console.log(numberObj);
   const closeBtn = document.querySelector(".lightBoxCLose");
   closeBtn.id = "closeModaleLightBox";
   const next = document.querySelector(".lightBoxNext");
@@ -95,23 +65,17 @@ function lightBox() {
   const firstFocusableElement =
     modaleLightBox.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
   const focusableContent = modaleLightBox.querySelectorAll(focusableElements);
-  const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
-
-  // console.log(firstFocusableElement);
-  // console.log(focusableContent);
-  // console.log(lastFocusableElement);
+  const lastFocusableElement = focusableContent[focusableContent.length - 1];
+  // get last element to be focused inside modal
   document.addEventListener("keydown", (e) => {
     let isTabPressed = e.keyCode === 9 || e.key === "enter";
-    // console.log(e.keyCode);
-    // console.log(e.key);
     if (!isTabPressed) {
       return;
     }
     if (e.shiftKey) {
-      // console.log(e.shiftKey);
       // if shift key pressed for shift + tab combination
       if (document.activeElement === firstFocusableElement) {
-        lastFocusableElement.focus(); // add focus for the last focusable element
+        lastFocusableElement.focus();
         e.preventDefault();
       }
     } else {
@@ -127,7 +91,6 @@ function lightBox() {
   firstFocusableElement.focus();
   /// Allow the focus inside the modale for accessibility End
 
-  ///////////// Function permettant d'utiliser la flèche de droite /////////////
   function nextSlide() {
     const selectObj = document.querySelectorAll(
       ".mediaSection img.mediaImage , .mediaSection video.movieMedia"
@@ -141,7 +104,6 @@ function lightBox() {
     let lightBoxImg = document.querySelector("#lightBoxImg");
     let lightBoxMovie = document.querySelector("#lightBoxMovie");
     let lightBoxActiveElement = null;
-    // console.log(count);
     if (selectObj[count].tagName == "IMG") {
       lightBoxMovie.style.display = "none";
       lightBoxImg.style.display = "block";
@@ -152,31 +114,24 @@ function lightBox() {
       lightBoxActiveElement = lightBoxMovie;
     }
 
-    // console.log(lightBoxActiveElement);
     lightBoxActiveElement.classList.remove("active");
 
     lightBoxActiveElement.classList.add("active");
     console.log(count);
-    // console.log(selectObj[count].src);
     lightBoxActiveElement.setAttribute("src", selectObj[count].src);
     titlelightBoxImg.textContent = selectTitles[count].innerText;
-
-    // titlelightBoxImg.textContent = selectObj[count].dataset.title;
   }
   next.addEventListener("click", nextSlide);
+  /// permet la navigation avec les fleches et escape
 
   document.addEventListener("keydown", (e) => {
-    // console.log(e.key);
     if (e.key === "ArrowRight") {
-      // console.log("nickel");
       nextSlide();
     }
     if (e.key === "Escape") {
-      // console.log("tchao");
       closeLightBox();
     }
     if (e.key === "ArrowLeft") {
-      // console.log("check ça");
       prevSlide();
     }
   });
@@ -190,14 +145,13 @@ function lightBox() {
     if (count > 0) {
       count--;
     } else {
-      // recupère le derier element de mon numberObj
+      // recupère le dernier element de mon numberObj
       count = numberObj - 1;
     }
     let lightBoxImg = document.querySelector("#lightBoxImg");
     let lightBoxMovie = document.querySelector("#lightBoxMovie");
     let lightBoxActiveElement = null;
     console.log(count);
-    // console.log(selectObj);
     if (selectObj[count].tagName == "IMG") {
       lightBoxMovie.style.display = "none";
       lightBoxImg.style.display = "block";
@@ -221,6 +175,8 @@ function lightBox() {
   function closeLightBox() {
     const modaleLightBox = document.querySelector(".lightboxModal");
     modaleLightBox.style.display = "none";
+    const selectTheFooter = document.getElementById("likePriceContainer");
+    selectTheFooter.style.visibility = "visible";
   }
 
   closeModaleLightBox.addEventListener("click", closeLightBox);
@@ -232,21 +188,24 @@ function lightBox() {
   });
 
   function initLightBoxEvent() {
+    const selectTheFooter = document.getElementById("likePriceContainer");
+    console.log(selectTheFooter);
     const selectObj = document.querySelectorAll(
       ".mediaSection img.mediaImage , .mediaSection video.movieMedia"
     );
 
     selectObj.forEach((objs) => {
-      // console.log(objs);
       objs.addEventListener("click", (event) => {
         console.log(event);
         showLightBox(event);
+        selectTheFooter.style.visibility = "hidden";
       });
       objs.addEventListener("keypress", (e) => {
         console.log(e);
         if (e.key == "Enter") {
           console.log(e.key);
           showLightBox(e);
+          selectTheFooter.style.visibility = "hidden";
         }
       });
     });
@@ -269,9 +228,8 @@ function lightBox() {
     const selectObj = document.querySelectorAll(
       ".mediaSection img.mediaImage , .mediaSection video.movieMedia"
     );
-    const selectTitles = document.querySelectorAll(".mediaSection .mediaTitle");
 
-    console.log(selectObj);
+    const selectTitles = document.querySelectorAll(".mediaSection .mediaTitle");
 
     if (event.target == imgMedia) {
       imgMedia.focus();
@@ -297,7 +255,6 @@ function lightBox() {
     lightBoxActiveElement.classList.remove("active");
     lightBoxActiveElement.classList.add("active");
 
-    // console.log(event.target.src);
     const modaleLightBox = document.querySelector(".lightboxModal");
     modaleLightBox.style.display = "block";
     firstFocusableElement.focus();
@@ -306,7 +263,6 @@ function lightBox() {
     count = Array.from(selectObj).indexOf(event.target);
 
     console.log(count);
-    console.log(selectTitles);
     titlelightBoxImg.textContent = selectTitles[count].innerText;
   }
   initLightBoxEvent();
